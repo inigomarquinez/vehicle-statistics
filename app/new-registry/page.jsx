533b28a43ae611b005aab97fd94c1fc2990058ac
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-09-27 19:49:36 +0200

    feat: basic empty skeleton

---
+++ app/new-registry/page.jsx
@@ -0,0 +1,3 @@
+export default function NewRegistry() {
+  <h1>New registry</h1>
+}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h2>My Dashboard</h2>
+    </main>
+  )
+}
